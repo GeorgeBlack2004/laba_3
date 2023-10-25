826357841bf0aced0eb2750ee0535bc257275a47
GeorgeBlack2004 <117338798+GeorgeBlack2004@users.noreply.github.com>
2023-10-25 11:11:53 +0300

    laba 3

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,4 @@
+count = 0
 with open("F1.txt", "w") as f1:
     while True:
         line = input("Введите строку: ")
@@ -7,16 +8,22 @@ with open("F1.txt", "w") as f1:
     f1.close()
 
 with open("F1.txt", "r") as f1, open("F2.txt", "w") as f2:
+    x = 0
     for line in f1:
+        x += 1
         words = line.split()
         if len(words) > 1 and words[1] in line:
-            f2.write(line)
+            if x != 1:
+                f2.write(line)
     f1.close()
     f2.close()
 
 with open("F2.txt", "r") as f2:
-    last_line = f2.readline()[-1]
-    digit_count = sum(1 for char in last_line if char.isdigit())
+    lines = list(f2)
+    last_line = lines[-1]
+    for i in last_line:
+        if i.isdigit():
+            count += 1
     f2.close()
 
-print("Количество цифр в последней строке файла F2: ", digit_count)
+print("Количество цифр в последней строке файла F2: ", count)
